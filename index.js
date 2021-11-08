23e36f056836224c69f5e068df7e4c9c610a01f9
Hidropolbat1 <Hidropolbat@gmail.com>
2021-11-08 19:02:22 +0700

    initial commit

--- index.js
+++ index.js
@@ -3,25 +3,35 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
-});
-
-app.get("/dashboard", (req, res) => {
-  res.render("dashboard");
 });
 
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.get("/monitoring", (req, res) => {
-  res.render("monitoring");
+app.post("/login", (req, res) => {
+  res.send("login");
 });
+
+app.get("/controlling", (req, res) => {
+  res.render("controlling");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({
+      message: 'Ohh you are lost, read the API documentation to find your way back home :)'
+  })
+})
 
 app.listen(8080, () => console.log("server listening on port 8080"));
