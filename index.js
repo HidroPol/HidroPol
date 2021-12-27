43f80e6ab77cb3b84050318a57e62d9614ebc104
HidroPol <hidropolbat@gmail.com>
2021-12-27 15:26:36 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
+require('dotenv').config()
 const express = require("express");
 const path = require("path");
-const PORT = 8080
-
+const bodyparser = require('body-parser')
+const cors = require('cors')
+const PORT = process.env.PORT || 8080
+const server = require('./routes/hidropolRoutes')
 const app = express();
 
+// cors
+app.use(cors())
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended : true}))
+app.use(bodyparser.json())
+
+// set view engine
 app.set("view engine", "ejs");
-
 app.set("views", path.join(__dirname, "views"));
+
+// load assets
 app.use(express.static("public"));
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-app.get("/", (req, res) => {
-  res.render("index", { title: "Hidropol" });
-});
-
-app.post("/login", (req, res) => {
-  res.send("login");
-});
-
-app.get("/controling", (req, res) => {
-  res.render("controling", { title: "Hidropol | Controlling" });
-});
-
-app.post("/controling", (req, res) => {
-  res.send("controling", { title: "Hidropol | Controlling" });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about", { title: "Hidropol | About" });
-});
-
-app.use((req, res, next) => {
-  res.render("404");
-});
+// load router
+app.use(server)
 
 app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
